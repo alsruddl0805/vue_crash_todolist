7132cda54cfa8bf65cb7e93edf4530adabb71ef4
alsruddl0805 <68004793+alsruddl0805@users.noreply.github.com>
2020-11-05 18:16:54 +0900

    todoList_limit_작업진행중

--- src/App.vue
+++ src/App.vue
@@ -29,11 +29,17 @@ export default {
       this.todos = this.todos.filter(todo => todo.id !== id);
     },
     addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
+      const { title, completed } = newTodo;
+      axios.post('http://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+        .then(res => this.todos = [...this.todos, res.data])
+        .catch(err => console.log(err));
     }
   },
   created() {
-    axios.get('http://jsonplaceholder.typicode.com/todos')
+    axios.get('http://jsonplaceholder.typicode.com/todos?_limit=5')
     .then(res => this.todos = res.data)
     .catch(err => console.log(err));
   }
