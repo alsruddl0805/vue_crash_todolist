4b04790a717bd59ef9732261c0ca301ed5733dc3
alsruddl0805 <68004793+alsruddl0805@users.noreply.github.com>
2020-11-06 12:20:28 +0900

    AddMenu (Home,About)

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,40 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos  v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+    <router-view/>
   </div>
 </template>
 
 <script>
 import Header from './components/layout/Header';
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
-import axios from 'axios';
 
-export default {
-  name: 'App',
-  components: {
-    Header,
-    Todos,
-    AddTodo
-  },
-  data() {
-    return {
-      todos: []
+  export default {
+    name: "app",
+    components: {
+      Header
     }
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
-    },
-    addTodo(newTodo) {
-      const { title, completed } = newTodo;
-      axios.post('http://jsonplaceholder.typicode.com/todos', {
-        title,
-        completed
-      })
-        .then(res => this.todos = [...this.todos, res.data])
-        .catch(err => console.log(err));
-    }
-  },
-  created() {
-    axios.get('http://jsonplaceholder.typicode.com/todos?_limit=5')
-    .then(res => this.todos = res.data)
-    .catch(err => console.log(err));
   }
-}
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+#nav {
+  padding: 30px;
+}
 
-  body {
-    font-family: Arial,Helvetica,sans-serif;
-    line-height: 1.4;
-  }
+#nav a {
+  font-weight: bold;
+  color: #fff;
+}
 
-  .btn {
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 7px 20px;
-    cursor: pointer;
-  }
-
-  .btn:hover {
-    background: #666;
-  }
-
+#nav a.router-link-exact-active {
+  color: #fff;
+}
 </style>
